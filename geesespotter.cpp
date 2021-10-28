74a07a145176ee35eccc913f54fee1d41d6ad235
Samer Rustum <samer@v1040-wn-rt-c-7-223.campus-dynamic.uwaterloo.ca>
2021-10-28 17:12:52 -0400

    work on printBoard function()

--- geesespotter.cpp
+++ geesespotter.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include "geesespotter_lib.h"
 //TODO:
-//createBoard();
-//hideBoard();
-//cleanBoard();
 //printBoard();
 //mark();
 char *createBoard(std::size_t xdim, std::size_t ydim)
@@ -20,50 +17,65 @@ void hideBoard(char *board, std::size_t xdim, std::size_t ydim)
 {
     for (int i{0}; i < xdim * ydim; ++i)
     {
-        board[i] += hiddenBit();
+        board[i] |= hiddenBit();
     }
+
+    // std::cout<<"HEE";
 }
 void cleanBoard(char *board)
 {
-    delete board;
+    delete[] board;
     board = nullptr;
 }
-void printBoard(char *board, std::size_t xdim, std::size_t ydim){
+void printBoard(char *board, std::size_t xdim, std::size_t ydim)
+{
     std::size_t index{0};
+    char second;
+    char num_geese;
     for (int i{0}; i < ydim; ++i)
     {
         for (int j{0}; j < xdim; ++j)
         {
             //converts a 2D index into a 1D index.
-            index = (xdim*j)+i;
+            index = (xdim * j) + i;
+            // second=((board[index]>>4)<<4);
+            // num_geese=(board[index]<<4);
             // std::cout<<(board[index]>>4)<< std::endl;
-            if ((board[index]>>4)<<4 == hiddenBit())
+            if ((board[index] & hiddenBit()) == hiddenBit())
             {
-                // std::cout<<"HEEE";
-                // printf("%x", '*');
-                std::cout << '*';
-            }else if((board[index]>>4)<<4 == hiddenBit()+markedBit()){
-                std::cout << 'M';
+                if ((board[index] & markedBit()) == markedBit())
+                {
+                    std::cout << 'M';
+                }else{
+                    std::cout << '*';
+                }
+            }
+            else
+            {
+                std::cout << (board[index] & valueMask());
             }
         }
-        std::cout<<std::endl;
+        std::cout << std::endl;
     }
 }
-    int reveal(char *board, std::size_t xdim, std::size_t ydim, std::size_t xloc, std::size_t yloc)
+int reveal(char *board, std::size_t xdim, std::size_t ydim, std::size_t xloc, std::size_t yloc)
+{
+    return 0;
+}
+int mark(char *board, std::size_t xdim, std::size_t ydim, std::size_t xloc, std::size_t yloc)
+{
+    std::size_t index{((xdim * xloc) + yloc)};
+    board[index] |= markedBit();
+    if ((board[index]&markedBit())==markedBit())
+    {
+        return 2;
+    }
+    else
     {
         return 0;
     }
-    int mark(char *board, std::size_t xdim, std::size_t ydim, std::size_t xloc, std::size_t yloc)
-    {
-        std::size_t index{((xdim*xloc)+yloc)};
-        if (((board[index]>>4)<<4 == markedBit() + hiddenBit()) || ((board[index]>>4)<<4 == markedBit())){
-            return 2;
-        }else{
-            board[index] += markedBit();
-            return 0;
-        }
-    }
-    bool isGameWon(char *board, std::size_t xdim, std::size_t ydim)
-    {
-        return false;
-    }
+}
+bool isGameWon(char *board, std::size_t xdim, std::size_t ydim)
+{
+    return false;
+}
--- test.cpp
+++ test.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
 int main(){
-    char c{0x02};
+    char c{0x20+0x10};
 
-    c+= 0x30;
-    printf("%x \n", c);
-    c>>=4;
-    printf("%x \n", c);
+    
+    std::cout<<c;
     return 0;
 }
 //100010
